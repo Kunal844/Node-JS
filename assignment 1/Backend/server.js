805da82573d3c6bdcdf1375cd1787d5eda3ed22d
Kunal <kunalhoney@gmail.com>
2026-02-13 13:42:13 +0530

    Push Test

---
+++ assignment 1/Backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const FILE_NAME = "data.txt";
+
+/* WRITE API */
+app.post("/write", (req, res) => {
+    const content = req.body.text;
+
+    fs.writeFile(FILE_NAME, content, (err) => {
+        if (err) return res.status(500).send("Error writing file");
+        res.send("Data written successfully");
+    });
+});
+
+/* READ API */
+app.get("/read", (req, res) => {
+    fs.readFile(FILE_NAME, "utf8", (err, data) => {
+        if (err) return res.status(500).send("Error reading file");
+        res.json({ data });
+    });
+});
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
